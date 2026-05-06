c984ab211afb4be8e59cc015c61efbe1e1a0c121
Shubham <firemare8@gmail.com>
2026-05-07 02:02:49 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/repo-name/', // Replace 'repo-name' with your GitHub repo name
+  base: '/',
   server: {
     host: "::",
     port: 8080,
